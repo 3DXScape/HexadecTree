757b7f140f6b4f0176ea99c7ae75d1a41e58ec5e
Steve Smyth <steve@opensiteplan.org>
2024-11-11 08:49:27 +0100

    Definitions

---
+++ HexadecTree/HexadecTree/HexadecaTree.cs
@@ -0,0 +1,17 @@
+﻿namespace HexadecaTree
+{
+    public class HexadecaTree
+    {
+        // given a x,y.z,t return a hdtree hash
+        // given a hdtree hash, retuen x,y,z,t
+        // define resolution of a hdtree as delta x, delta y, delta z, delta st
+        // given speed of causal interaction (sci) in mm/sec, return delta t
+        // note: space units are mm
+        // note: clock-time units are picoseconds
+        // note: time units are mm = sci * delta t, where delta t is in picoseconds
+        //        for example, static environments sci = 0 and delta t  is zero
+        //        for example. for automobiles sci = (100 m/sec * 1000 mm/m) / 1 000 000 000 000 = 10 000 000 mm/ps
+        //        for example, for light sci = (300 000 m/sec * 1000 mm/m) / 1 000 000 000 000 = 30 mm / s
+
+    }
+}
